423af65cc1e7de8574f82b386bfc9502ef1fde56
Mohamad Khajezade <khajezade.mohamad@gmail.com>
2024-03-01 01:39:17 -0800

    Lab Project Initiated

---
+++ app/src/main/java/com/mastercoding/volumeareaapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mastercoding.volumeareaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 1- AdapterView: GridView
+
+
+    // 2- Data Source: ArrayList<Shape>
+
+
+    // 3- Adapter: MyCustomAdapter
+
+}
